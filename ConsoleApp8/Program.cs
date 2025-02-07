33377182d64cae90db02e97f596af680021d25c1
Georgi Georgiev <Georgi Georgiev@DESKTOP-9FI4TKH>
2025-02-07 15:26:34 +0200

    Add project files.

---
+++ ConsoleApp8/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Type the number");
+        int n = int.Parse(Console.ReadLine());
+        int left_sum = 0;
+        int right_sum = 0;
+
+        for (int i = 0; i < n; i++)
+            left_sum += Convert.ToInt32(Console.ReadLine());
+
+        for (int i = 0; i < n; i++)
+            right_sum += Convert.ToInt32(Console.ReadLine());
+
+        if (left_sum == right_sum)
+            Console.WriteLine($"Yes, sum = {left_sum}");
+        else
+            Console.WriteLine($"No, difference = {Math.Abs(left_sum - right_sum)}");
+    }
+}
